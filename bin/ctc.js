847b6add346af1b6c5a92a344001d2cdf45c3591
Keith M Wesolowski <wesolows@foobazco.org>
2014-08-26 18:33:18 +0000

    TOOLS-607 I, for one, welcome my new open source license

--- bin/ctc.js
+++ bin/ctc.js
@@ -1,7 +1,12 @@
 #! /opt/local/bin/node
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
 /*
- * Copyright 2014 Joyent, Inc.  All rights reserved.
+ * Copyright (c) 2014, Joyent, Inc.
  */
 
 var CT_simulation = require('../lib/CT_simulation.js');
